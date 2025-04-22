485f33130fb992a61ef7568a34dae2e471b2e9b8
Jro Luttringer <jr.luttringer@unistra.fr>
2025-04-22 13:52:22 +0200

    working website

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
 
+const isProduction = process.env.NODE_ENV === 'production';
+const basePath = isProduction ? '/mariage-tania-jr' : '';
+
 const nextConfig = {
     output: 'export',
     distDir: 'docs',  // Specify the output directory as 'docs'
+    basePath: basePath,  // Define the base path for GitHub Pages
+    assetPrefix: basePath,  // Use the same base path for assets
     images: {
       unoptimized: true, // required if you use `next/image`
     },
+    trailingSlash: true,
 }
-
-// module.exports = {
-//     eslint: {
-//       ignoreDuringBuilds: true,
-//     },
-// }
 
 export default nextConfig;

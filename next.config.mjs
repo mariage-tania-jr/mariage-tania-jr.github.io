af51e8592681f365c4b7cb5b445fd30993513f82
Jro Luttringer <jr.luttringer@unistra.fr>
2025-04-22 17:08:55 +0200

    working website

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 // For GitHub Pages deployment, we need to use a specific configuration
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'mariage-tania-jr';
 
 const nextConfig = {
     output: 'export',
     distDir: 'docs',
-    
-    // Use basePath for GitHub Pages
-    basePath: isProd ? `/${repoName}` : '',
-    
-    // Add trailing slash for better path resolution
-    trailingSlash: true,
-    
-    // This is the key setting for GitHub Pages
-    assetPrefix: isProd ? `/${repoName}/` : '',
-    
+
     // Required for Next.js Image component in static export
     images: {
       unoptimized: true,
